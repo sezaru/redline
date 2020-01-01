67c5fb56a0b1f5bb6b85b3a058668839be001a19
Eduardo Barreto Alexandre <eduardo.alexandre@siemens-healthineers.com>
2020-01-01 04:03:26 -0300

    Fixes formatter rules

--- .formatter.exs
+++ .formatter.exs
@@ -1,10 +1,12 @@
-export_local_without_parens = [
+locals_without_parens = [
   step: 1,
   step: 2
 ]
 
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  local_without_parens: export_local_without_parens,
-  export: [local_without_parens: export_local_without_parens]
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
+  ]
 ]
